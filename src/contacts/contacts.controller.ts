75fecd6bb1d7bb709c07eb884cb6afadf457f895
Guido <38866056+guidoperman1986@users.noreply.github.com>
2023-09-11 21:41:06 -0300

    auth guard added to contacts

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -6,15 +6,18 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactsService.create(createContactDto);
@@ -30,11 +33,13 @@ export class ContactsController {
     return this.contactsService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactsService.update(+id, updateContactDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.contactsService.remove(id);
--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -4,13 +4,18 @@ import { ContactsController } from './contacts.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contact, ContactSchema } from './entities/contact.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { User, UserSchema } from 'src/auth/entities/user.entity';
 
 @Module({
   controllers: [ContactsController],
-  providers: [ContactsService],
+  providers: [ContactsService, AuthService],
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
+    MongooseModule.forFeature([
+      { name: Contact.name, schema: ContactSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
 })
 export class ContactsModule {}
